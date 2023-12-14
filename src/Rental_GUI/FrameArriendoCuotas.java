dc258b931295b6fea74cc98616676a65bbedc60c
Karmacode00 <alafertteo@gmail.com>
2023-12-14 16:38:39 -0300

    pequeño avance

--- src/Rental_GUI/FrameArriendoCuotas.java
+++ src/Rental_GUI/FrameArriendoCuotas.java
@@ -2,21 +2,48 @@ package Rental_GUI;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class FrameArriendoCuotas extends JFrame{
+public class FrameArriendoCuotas extends JFrame implements ActionListener{
+        JButton ingresoCliente;
+
+
 
         FrameArriendoCuotas() {
 
-                JLabel label =  new JLabel("Arriendo con Cuotas");
-                label.setVerticalTextPosition(JLabel.TOP);
-                label.setHorizontalTextPosition(JLabel.CENTER);
+                //JPanel panel = new JPanel();
+
+                JLabel label =  new JLabel("ARRIENDO CON CUOTAS");
+
                 this.setTitle("Arriendo de vehículos");
                 this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Cerrar programa con la x
                 this.setResizable(false); // Prevenir que se reajuste la ventana
-                this.setSize(500, 400); // Tamaño ventana
+                this.setSize(800, 600); // Tamaño ventana
                 this.setVisible(true); // Que sea visible
-                this.getContentPane().setBackground(new Color(128, 128, 128));
+                this.getContentPane().setBackground(new Color(220, 220, 220));
                 this.add(label);
+                //this.pack(); // ajusta tamaño a los componentes
 
+                //label.setVerticalTextPosition(JLabel.TOP);
+                //label.setHorizontalTextPosition(JLabel.CENTER);
+                label.setHorizontalAlignment(JLabel.CENTER);
+                label.setVerticalAlignment(JLabel.TOP);
+                label.setFont(new Font("Arial", Font.PLAIN, 20));
+
+                ingresoCliente = new JButton();
+                ingresoCliente.setBounds(200, 200, 100, 50);
+                ingresoCliente.addActionListener(this); // Opcional sería usar expresión lambda (e -> System.out.println("Hello World))
+                this.add(ingresoCliente);
+
+
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+                if(e.getSource()== ingresoCliente){
+                        System.out.println("Hello world");
+
+                }
         }
 }
